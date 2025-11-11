9b61f4d14d59cd67ded9a528183ab3cff77c42e4
vbresan <288855+vbresan@users.noreply.github.com>
2025-11-11 16:20:47 +0100

    Adding donation button

---
+++ app/src/google/java/biz/binarysolutions/imageconverter/FlavorSpecific.java
@@ -0,0 +1,14 @@
+package biz.binarysolutions.imageconverter;
+
+import android.app.Activity;
+
+public class FlavorSpecific {
+
+    public FlavorSpecific(Activity activity) {
+        // do nothing
+    }
+
+    public void initialize() {
+        // do nothing
+    }
+}
